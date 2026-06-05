7310b4912f602c5824f49e3cda287db9e052860e
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-06-05 12:53:37 +0200

    add variables and style header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,49 +5,48 @@ import { UserDTO } from "../models/User";
 const Header = async () => {
   const authUser: UserDTO | null = await checkAuth();
   return (
-    <header className="bg-white shadow px-20">
-      <div className="container mx-auto px-4 py-6 flex items-center justify-between">
-        <nav>
+    <header className="shadow px-20">
+      <div className="container mx-auto px-4 py-6 flex justify-between">
+        <nav className="grow-2 flex items-center justify-center">
           <Link href="/">
-            <span className="rounded p-3 text-white bg-gray-500 hover:bg-gray-700 mx-4">
+            <span className=" p-3 mx-4 font-bold text-text-black hover:underline">
               VÄLKOMNA
             </span>
           </Link>
           <Link href="/schedule">
-            <span className="rounded p-3 text-white bg-gray-500 hover:bg-gray-700 mx-4">
+            <span className="rounded p-3 mx-4 font-bold text-text-black hover:underline">
               UPPLÄGG
             </span>
           </Link>
 
           <Link href="/osa">
-            <span className="rounded p-3 text-white bg-gray-500 hover:bg-gray-700 mx-4">
+            <span className="rounded p-3 mx-4 font-bold text-text-black hover:underline">
               OSA
             </span>
           </Link>
           <Link href="/info">
-            <span className="rounded p-3 text-white bg-gray-500 hover:bg-gray-700 mx-4">
+            <span className="rounded p-3 mx-4 font-bold text-text-black hover:underline">
               INFO
             </span>
           </Link>
           {authUser && (
             <Link href="/guests">
-              <span className="rounded p-3 text-white bg-gray-500 hover:bg-gray-700 mx-4">
+              <span className="rounded p-3 mx-4 font-bold text-text-black hover:underline">
                 GÄSTLISTAN
               </span>
             </Link>
           )}
         </nav>
         {authUser ? (
-          <p>Du är inloggad som, {authUser.username}!</p>
+          <span className="justify-self-end p-3 mx-4  text-text-black">
+            Du är inloggad som, {authUser.username}!
+          </span>
         ) : (
-          <div>
-            <p>Du är inte inloggad.</p>
-            <Link href="/login">
-              <span className="rounded p-1 text-white bg-gray-500 hover:bg-gray-700 mx-4">
-                Logga in
-              </span>
-            </Link>
-          </div>
+          <Link href="/login">
+            <span className=" p-3 mx-4 text-text-black hover:underline">
+              Logga in
+            </span>
+          </Link>
         )}
       </div>
     </header>
