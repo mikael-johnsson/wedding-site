9ea1e48b145b22e462f79f55771484347d756dde
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-06-08 14:08:13 +0200

    make boende page responsive

--- app/boende/page.tsx
+++ app/boende/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-
 const LivingAndTransportPage = () => {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen px-10">
       <h1 className="text-4xl font-bold text-center mt-10">
         BOENDE & TRANSPORT
       </h1>
-      <div className="flex">
-        <div className=" w-300 mx-auto px-4 py-10">
+      <div className="mx-auto flex flex-col gap-6 px-4 py-10 lg:flex-row lg:items-start lg:justify-center lg:gap-10">
+        <div className="w-full max-w-2xl px-4 py-10 lg:w-1/2">
           <h2 className="text-2xl font-bold mb-10 text-center">Boende</h2>
           <div className="flex flex-col gap-4">
             <p>
@@ -24,7 +22,7 @@ const LivingAndTransportPage = () => {
             </div>
           </div>
         </div>
-        <div className="container w-300 mx-auto px-4 py-10">
+        <div className="w-full max-w-2xl px-4 py-10 lg:w-1/2">
           <h2 className="text-2xl font-bold text-center mb-10">Transport</h2>
           <div className="flex flex-col gap-4">
             <p>Egen bil är att föredra.</p>
