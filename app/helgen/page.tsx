2ccd11ed63d63f86603f404cba5f1a78df98a715
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-06-08 14:06:50 +0200

    make helgen page responsive

--- app/helgen/page.tsx
+++ app/helgen/page.tsx
@@ -4,15 +4,15 @@ const WeekendPage = () => {
   return (
     <main className="min-h-screen">
       <h1 className="text-4xl font-bold text-center mt-10">HELGEN</h1>
-      <div className="container mx-auto px-4 py-6 flex gap-6 justify-evenly">
+      <div className="container mx-auto flex flex-col gap-4 px-4 py-6 lg:flex-row lg:justify-evenly">
         {/* FREDAG */}
-        <div className="border rounded p-6 h-180 w-1/4">
+        <div className="w-full rounded border p-6 lg:w-[30%]">
           <Image
             src="/herrgård-sketchad.png"
             alt="Wedding"
-            className="object-contain"
-            width={800}
-            height={1000}
+            className="object-contain mx-auto"
+            width={600}
+            height={900}
           />
           <h2 className="text-2xl font-extrabold text-center my-10">FREDAG</h2>
           <p className="text-md font-bold text-center ">
@@ -27,13 +27,13 @@ const WeekendPage = () => {
         </div>
 
         {/* LÖRDAG */}
-        <div className="border rounded p-6 h-180 w-1/4">
+        <div className="w-full rounded border p-6 lg:w-[30%]">
           <Image
             src="/varnamo_kyrka.jpg"
             alt="Wedding"
-            className="object-contain"
-            width={800}
-            height={1000}
+            className="object-contain mx-auto"
+            width={600}
+            height={900}
           />
           <h2 className="text-2xl font-extrabold text-center my-10">LÖRDAG</h2>
           <p className="text-md font-bold text-center ">
@@ -48,13 +48,13 @@ const WeekendPage = () => {
         </div>
 
         {/* SÖNDAG */}
-        <div className="border rounded p-6 h-180 w-1/4">
+        <div className="w-full rounded border p-6 lg:w-[30%]">
           <Image
             src="/herrgård-sketchad.png"
             alt="Wedding"
-            className="object-contain"
-            width={800}
-            height={1000}
+            className="object-contain mx-auto"
+            width={600}
+            height={900}
           />
           <h2 className="text-2xl font-extrabold text-center my-10">SÖNDAG</h2>
           <p className="text-md font-bold text-center ">
