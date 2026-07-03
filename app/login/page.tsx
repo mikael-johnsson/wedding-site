4addb4bd2c1e84a2512f47b7525ea1b329313694
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-07-03 10:19:13 +0200

    style login page1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,10 +2,10 @@ import { loginUser } from "../actions/UserActions";
 
 const LoginPage = () => {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+    <main className="min-h-screen flex justify-center">
       <form
         action={loginUser}
-        className="bg-white p-8 rounded shadow-md w-full max-w-md flex flex-col gap-4"
+        className="p-8 rounded shadow-lg w-full h-fit max-w-md flex flex-col gap-4 mt-30"
       >
         <h2 className="text-2xl font-heading mb-4">Logga in</h2>
         <div className="flex flex-col gap-1">
@@ -41,7 +41,7 @@ const LoginPage = () => {
         </div>
 
         <button
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-60"
+          className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-60"
           type="submit"
         >
           Logga in
