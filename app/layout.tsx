a76409ca5f2e5a67540f1e0af23fae3ebbab13b4
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-08-17 17:51:17 +0200

    update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={` h-full antialiased`}>
+    <html lang="en" className={`h-full antialiased`}>
       <body>
         <Header />
         {children}
